5f996411a7591c56c93f04c9b81acf125baa1d89
yahyathegreat <yahyamardan1234@gmail.com>
2024-11-16 10:59:50 +0500

    word matching

--- main.py
+++ main.py
@@ -7,3 +7,14 @@ print("triples")
 alist = [100, 200, 300, 400, 500]
 alist = alist[::-1]
 print(alist,"\n")
+def match_words(words):
+    ctr = 0
+    lst = []
+    for word in words:
+            if len(word) > 1 and word[0]== word[-1]:
+                    ctr +=1
+                    lst.append(word)
+    print("list of words with first and last character same\n",lst)
+    return ctr
+count = match_words(['abc','cfc','xyz','aba','1221'])
+print("number of words having first and last character same:",count)
