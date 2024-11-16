e0c32a37ddfd9c4d7e8946c9ad7aee1d6b9a9429
yahyathegreat <yahyamardan1234@gmail.com>
2024-11-16 10:48:41 +0500

    lists

---
+++ main.py
@@ -0,0 +1,9 @@
+empty_list = []
+print(empty_list)
+numbers = [1, 2, 3, 4, 5] * 3
+print("numbers")
+triples = [1, 2, 3]
+print("triples")
+alist = [100, 200, 300, 400, 500]
+alist = alist[::-1]
+print(alist,"\n")
